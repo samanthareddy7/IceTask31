0bd7988edbc6661ab08d3f3a46e6180bec43d843
samanthareddy7 <st10454507@vcconnect.edu.za>
2025-03-13 14:43:55 +0200

    Shape Class

--- IceTask3/Rectangle.cs
+++ IceTask3/Rectangle.cs
@@ -1,6 +1,24 @@
 namespace IceTask3;
 
-public class Rectangle
+public class Rectangle : Shape, ICalculateArea
 {
-    
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(string name, double width, double height) : base(name)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public double CalculateArea()
+    {
+        return Width * Height;
+    }
+
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine($"Width: {Width}, Height: {Height}, Area: {CalculateArea()}");
+    }
 }
