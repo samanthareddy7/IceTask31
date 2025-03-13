3ca8ebea2bb28a02c95d137dab76103d3232e153
samanthareddy7 <st10454507@vcconnect.edu.za>
2025-03-13 14:38:19 +0200

    Shape Class

--- IceTask3/Shape.cs
+++ IceTask3/Shape.cs
@@ -1,6 +1,17 @@
 namespace IceTask3;
 
-public class Shape
+public abstract class Shape
 {
-    
+    public string Name { get; set; }
+
+    public Shape(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void Display()
+    {
+        Console.WriteLine($"Shape: {Name}");
+    }
 }
+
