7b9db5aba68b997a72a0ec180e4ab2585dff599e
samanthareddy7 <st10454507@vcconnect.edu.za>
2025-03-13 19:28:47 +0200

    Circle Class

--- IceTask3/Circle.cs
+++ IceTask3/Circle.cs
@@ -1,6 +1,22 @@
 namespace IceTask3;
 
-public class Circle
+public class Circle : Shape, ICalculateArea
 {
-    
+    public double Radius { get; set; }
+
+    public Circle(string name, double radius) : base(name)
+    {
+        Radius = radius;
+    }
+
+    public double CalculateArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+
+    public override void Display()
+    {
+        base.Display();
+        Console.WriteLine($"Radius: {Radius}, Area: {CalculateArea()}");
+    }
 }
--- IceTask3/Program.cs
+++ IceTask3/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Rectangle rect = new Rectangle("Rectangle", 5, 10);
+        rect.Display();
+
+        Circle circ = new Circle("Circle", 7);
+        circ.Display();
     }
 }
